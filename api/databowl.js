05719805e9cc71fca04a80ece3f597d82bce5066
CRSAds <yuri@crsadvertising.com>
2025-04-20 18:27:12 +0200

    Update databowl.js

--- api/databowl.js
+++ api/databowl.js
@@ -16,21 +16,23 @@ export default async function handler(req, res) {
 
   const params = new URLSearchParams();
   params.append("cid", "4786");
-  params.append("sid", "34");
-  params.append("optin_email", "1");
-  params.append("optin_sms", "0");
-  params.append("optin_phone", "1");
+params.append("sid", "34");
+params.append("optin_email", "1");
+params.append("optin_sms", "0");
+params.append("optin_phone", "1");
 
-  params.append("first_name", body.first_name || "");
-  params.append("last_name", body.last_name || "");
-  params.append("email", body.email || "");
-  params.append("phone", body.phone || "");
-  params.append("postcode", body.postcode || "");
-  params.append("address1", body.address1 || "");
-  params.append("town", body.town || "");
-  params.append("f_1684_sub_id", body.sub_id || "");
-  params.append("f_1685_aff_id", body.aff_id || "");
-  params.append("f_1322_transaction_id", body.transaction_id || "");
+// Gebruik de correcte veldnamen hier:
+params.append("f_3_firstname", body.first_name || "");
+params.append("f_4_lastname", body.last_name || "");
+params.append("f_1_email", body.email || "");
+params.append("f_12_phone1", body.phone || "");
+params.append("f_11_postcode", body.postcode || "");
+params.append("f_6_address1", body.address1 || "");
+params.append("f_9_towncity", body.town || "");
+
+params.append("f_1684_sub_id", body.sub_id || "");
+params.append("f_1685_aff_id", body.aff_id || "");
+params.append("f_1322_transaction_id", body.transaction_id || "");
 
   try {
     const response = await fetch("https://crsadvertising.databowl.com/api/v1/lead", {
